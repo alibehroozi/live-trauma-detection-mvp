8c32b1f6672508e5b92e345b14f743a3144afdea
zeinabsayyadi <zeinsa0158@gmail.com>
2022-01-26 00:36:11 +0330

    Update send.py

--- backend/send.py
+++ backend/send.py
@@ -5,6 +5,16 @@ import os
 
 WORKINGDIR = os.path.abspath("../aiworkingdir")
 
+app = Client(
+    session_name="myclient",
+    proxy=dict(
+        hostname="185.235.40.252",
+        port=7090,
+        # username="<your_username>",
+        # password="<your_password>"
+    )
+)
+
 api_id= 1614783                                             # account 09173898626
 api_hash = '65b3a547d9d2f0b6145a8ad3896e6313'               # account 09173898626
 
